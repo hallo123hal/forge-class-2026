68db2f4e92b94b628110f1e650c91cec6e97502b
tungns <sontungnguyen16@gmail.com>
2026-05-18 11:34:26 +0700

    Config vite dev server

--- Custom-UI-Issue-Panel/static/hello-world/vite.config.js
+++ Custom-UI-Issue-Panel/static/hello-world/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './',
+  server: {
+    port: 5173,
+    strictPort: true,
+    cors: true,
+  },
   build: {
     outDir: 'build',
     emptyOutDir: true,
